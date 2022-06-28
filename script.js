11547b077fd0811f6bde0c26a848d2abb3bf6332
philippe <ppereira@simplon.co>
2022-06-28 15:31:12 +0200

    added script

---
+++ script.js
@@ -0,0 +1,12 @@
+const shi = document.getElementById('shi');
+const fu = document.getElementById('fu');
+const mi = document.getElementById('mi');
+const player = document.getElementById('player');
+const ia = document.getElementById('ia');
+const command = document.getElementById('command');
+
+shi.addEventListener('click', () => console.log('shi'));
+fu.addEventListener('click', () => console.log('fu'));
+mi.addEventListener('click', () => console.log('mi'));
+
+// Pour le reste, a vous de jouer
